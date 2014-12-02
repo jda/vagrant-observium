1758b3130b971578b8eccad6d1cc4ddac21081a0
Jon Auer <jauer@corp.netwurx.net>
2014-12-02 16:32:12 -0600

    update for observium changes, move rrd out of observium dir so we can use it as a synced folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+mkdir /root/.subversion
+
+echo -e "[global]\nstore-plaintext-passwords = yes\n" > /root/.subversion/servers
+
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y mysql-server-5.5 libapache2-mod-php5 \
   php5-cli php5-mysql vim snmp-mibs-downloader \
-  php5-gd php5-snmp php-pear snmp graphviz subversion mysql-server \
+  php5-gd php5-snmp php5-mcrypt php-pear snmp graphviz subversion mysql-server \
   mysql-client rrdtool fping imagemagick whois mtr-tiny nmap ipmitool
 
 if [ ! -f "/opt/observium" ]; then
-  svn co http://observium.org/svn/observer/trunk/ /opt/observium
+  svn --username `cat /opt/misc/svn.user` --password `cat /opt/misc/svn.passwd` co http://svn.observium.org/svn/observium/trunk/ /opt/observium
 else
   svn up
 fi
@@ -17,8 +21,9 @@ mysql -u root -e "CREATE DATABASE observium;"
 
 cat config.php.default | sed s/PASSWORD//g | sed s/USERNAME/root/g > config.php
 
-php includes/sql-schema/update.php
-mkdir graphs rrd
+php includes/update/update.php
+
+mkdir graphs rrd logs
 chown www-data:www-data graphs rrd
 chmod -R 777 graphs rrd
 
